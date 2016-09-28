34359e6edad7f99af366e7fda3811ae701a3ff96
Ken Dreyer <kdreyer@redhat.com>
2016-09-28 13:41:14 -0600

    setup.py: fix flake8 spaces around keywords
    resolves E251 unexpected spaces around keyword / parameter equals

--- ceph-iscsi-config/setup.py
+++ ceph-iscsi-config/setup.py
@@ -9,15 +9,15 @@ long_description = f.read().strip()
 f.close()
 
 setup(
-    name = "ceph_iscsi_config",
-    version= "0.5",
-    description= "Common classes/functions used to configure iscsi gateways backed by ceph/kRBD",
-    long_description = long_description,
-    author = "Paul Cuzner",
-    author_email = "pcuzner@redhat.com",
-    url = "http://github.com/pcuzner/igw-ceph-ansible",
-    license = "GPLv3",
-    packages = [
+    name="ceph_iscsi_config",
+    version="0.5",
+    description="Common classes/functions used to configure iscsi gateways backed by ceph/kRBD",
+    long_description=long_description,
+    author="Paul Cuzner",
+    author_email="pcuzner@redhat.com",
+    url="http://github.com/pcuzner/igw-ceph-ansible",
+    license="GPLv3",
+    packages=[
         "ceph_iscsi_config"
         ]
 )
